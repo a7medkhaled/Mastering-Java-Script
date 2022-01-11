167fe1fbbb3e6aed403515452f8c4d30e1f4b2d9
applebuy <samer.kador@qutip.com>
2022-01-12 00:35:33 +0200

    arrays

--- conditions/index.js
+++ conditions/index.js
@@ -19,3 +19,33 @@ x = NaN;
 if (!x) {
   console.log("NaN is falsy");
 }
+
+//truthy values
+
+x = {};
+if (x) {
+  console.log("empty object is truthy");
+}
+
+x = [];
+if (x) {
+  console.log("empty array is truthy");
+}
+
+// switch statment without break
+var tier = "nsfw deck";
+var output = "You’ll receive ";
+
+switch (tier) {
+  case "deck of legends":
+    output += "a custom card, ";
+  case "collector's deck":
+    output += "a signed version of the Exploding Kittens deck, ";
+  case "nsfw deck":
+    output +=
+      "one copy of the NSFW (Not Safe for Work) Exploding Kittens card game and ";
+  default:
+    output += "one copy of the Exploding Kittens card game.";
+}
+
+console.log(output);
