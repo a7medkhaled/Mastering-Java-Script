ca0e234e6e2f34d1fca1f0b4a327fa4ac10bda03
applebuy <samer.kador@qutip.com>
2022-01-11 12:06:21 +0200

    equality

---
+++ equality/index.js
@@ -0,0 +1,29 @@
+console.log(1 == "1");
+
+console.log(1 == true);
+console.log("   " == false);
+
+console.log("vgvgv" * 12);
+//strict equality
+console.log(1 === "1");
+
+console.log(1 === true);
+console.log("   " === false);
+
+/*
+
+
+NaN --> not a number 
+
+like when do the following 
+"vgvgv" *12
+
+
+null is not as undefined 
+
+undefined if not assigned 
+null must be assigned 
+
+null value of nothing 
+undefined absence of value 
+*/
